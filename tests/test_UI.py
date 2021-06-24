e3cec922e6e6478b687c1727fc99ba1e80927f52
george <george.duffett@icloud.com>
2021-06-24 11:46:28 +0100

    Specified UI tests

---
+++ tests/test_UI.py
@@ -0,0 +1,21 @@
+import groceries as gc  # The code to test
+import unittest         # The test framework
+
+
+class TestUI(unittest.TestCase):
+
+    def setUp(self) -> None:
+        '''Runs the flask application, visits the page in chrome'''
+        return super().setUp()
+
+
+    def test_saveRecipe(self):
+        pass
+
+
+    def test_loadRecipe(self):
+        pass
+
+
+    def test_combineRecipes(self):
+        pass

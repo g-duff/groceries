11173fb7a0cb87e1c20dc9250d6bc93a39596522
george <george.duffett@icloud.com>
2021-07-07 12:14:24 +0100

    Load recipe func and test

--- groceries.py
+++ groceries.py
@@ -10,8 +10,10 @@ class Ingredient:
         except AttributeError:
             self.meal = None
 
+
     def __str__(self) -> str:
         return f"{self.quantity} {self.unit} of {self.name}"
+
 
     def __repr__(self) -> str:
         return f"Ingredient({self.name}, {self.quantity}, {self.unit}, \
@@ -20,12 +22,15 @@ class Ingredient:
     
 
 class ShoppingList:
+
     def __init__(self, items):
         self.items = items
+
 
     def getCategory(self, catgory):
         categorizedItems = [i for i in self.items if i.category==catgory]
         return categorizedItems
+
 
     def sumDuplicates(self) -> None:
 
@@ -39,3 +44,10 @@ class ShoppingList:
 
     def __str__(self) -> str:
         return "\n".join(str(i) for i in self.items)
+
+
+    @staticmethod
+    def load(fpath):
+        with open(fpath, 'r') as open_meal:
+            ingredient_list = [Ingredient(*line.split(',')) for line in open_meal]
+        return ShoppingList(ingredient_list)
--- tests/test_groceries.py
+++ tests/test_groceries.py
@@ -13,6 +13,24 @@ class TestIngredients(unittest.TestCase):
 
 class TestGroceries(unittest.TestCase):
 
+    def test_load(self):
+        ''' Load a recipe from a filepath'''
+
+        inString = [
+            "1.0 tin of tinned tomatoes",
+            "1.0 tin of kidney beans",
+            "1.0 tin of black beans",
+            "1.0 # of onion",
+            "1.0 cloves of garlic"
+        ]
+
+        answerString = "\n".join(inString)
+
+        questionList = gc.ShoppingList.load("recipes/chilli.csv")
+
+        self.assertEqual(str(questionList), answerString)
+ 
+
     def test_getCategories(self):
         '''Get items in a given category'''
 

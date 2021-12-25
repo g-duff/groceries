be9d23a0a145602deccde178e12772d9c8ca9ac2
george <george.duffett@icloud.com>
2021-12-25 19:23:47 +0000

    Remove csv calls from database layer

--- databaseLayer/databaseService.py
+++ databaseLayer/databaseService.py
@@ -3,16 +3,6 @@ from pathlib import Path
 import sqlite3
 import groceries 
 
-def load(fpath):
-    with open(fpath, 'r') as open_meal:
-        ingredient_list = [groceries.Ingredient(*line.split(',')) for line in open_meal]
-    return ingredient_list
-
-
-def mealList(recipePath = "databaseLayer/recipes"):
-    return [f for f in Path(recipePath).glob("*") if f.is_file()]
-
-
 class DatabaseService():
 
     def __init__(self, databasePath: Path = Path("./databaseLayer/recipes.db")):
